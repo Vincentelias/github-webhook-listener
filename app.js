80c75cd86afc7782cb71904553f8db74b9983a65
Vincent <vincent.elias@hotmail.com>
2024-11-08 15:41:28 +0000

    updated logging

--- app.js
+++ app.js
@@ -18,20 +18,24 @@ const PROJECTS_FOLDER = process.env.PROJECTS_FOLDER
 // Function to verify GitHub webhook signature
 const verifySignature = (req) => {
     const signature = req.headers['x-hub-signature'];
-    console.log('webhook secret', GITHUB_WEBHOOK_SECRET)
     const hash = `sha1=${crypto.createHmac('sha1', GITHUB_WEBHOOK_SECRET).update(JSON.stringify(req.body)).digest('hex')}`;
     return signature === hash;
 };
 
 // Webhook endpoint
 app.post('/webhook', (req, res) => {
+    console.log("Received call from Github'")
     if (!verifySignature(req)) {
+        console.log("Unable to verify signature, make sure your github webhook secret is set correctly")
         return res.status(401).send('Unauthorized');
     }
+
+    console.log("Successfully verified signature")
 
     const repoName = req.body.repository.name; // Get the repository name from the payload
     const scriptToExecute = `${PROJECTS_FOLDER}/${repoName}/on-push-to-repo.sh`; // Path to your update script
 
+    console.log('Executing script in project folder')
     exec(scriptToExecute, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
