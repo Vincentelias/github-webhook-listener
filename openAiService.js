f8c3d2591885b2a6836950d6fd9050f30d9ca73d
Vincent <vincent.elias@hotmail.com>
2024-12-15 19:22:03 +0000

    modfied prompt

--- openAiService.js
+++ openAiService.js
@@ -26,14 +26,17 @@ async function summarizeErrorLog(errorLog) {
                 {
                     role: 'user',
                     content: `
-                        Summarize the following error log and give me the root cause of the error 
+                        Summarize the following error log and give me the root cause of the error as concise as possible.
+                        Ignore and do not show me warnings or other problems. If there is a root cause, only the root cause 
                         (the most specific one which is most likely the root cause of why the build is failing). 
                         Investigate and try to find the root cause. 
-                        If there are multiple errors, give me the root cause of EACH error, don't skip any. 
-                        Don't give me info about the on-push-to-repo.sh script, only the error log of the project. 
+                        If there are multiple errors (which are actually causing the build to fail), give me the root cause of EACH error, don't skip any. 
+                        Don't give me info about the on-push-to-repo.sh script, only the error log of the project.
                         Send the message in Markdown, it's for a telegram message. Don't use emojis. 
                         If you see docker run scripts, ignore them. 
                         Ignore all things related to the docker scripts, these are not the root cause.
+                        Do everything in a concise way, as short as possible without giving me extra data or information which I don't need.
+                        I am an experienced programmer, so if you can just explain to me the issue that's enough.
                         Try to find it based on the log This is the error log:
                         ${errorLog}`
                 }
